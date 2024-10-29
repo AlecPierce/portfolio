40ce0dbab889c461f6dd03830771594096afe84c
Alec <alecpierce19@gmail.com>
2024-10-28 21:25:33 -0500

    created main-menu file

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './menus/main-menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
