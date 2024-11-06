0b9adb091d91680d3d68f240fb23a289acfa2947
Alec <alecpierce19@gmail.com>
2024-11-05 21:56:30 -0600

    made standalone component for hero-class and updated styling

--- web-app/src/app/menus/main-menu.ts
+++ web-app/src/app/menus/main-menu.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Hero } from '../classes/hero';
+import { HeroClassComponent } from '../components/heroClass.component';
 
 @Component({
     selector: 'app-main-menu',
     standalone: true,
     template: `
-        <div class="flex items-center justify-center h-screen">
-            <div class="bg-gray-800 text-black p-6 rounded-lg shadow-lg">
-                <h1 class="text-2xl font-bold mb-4">Hero Class Menu</h1>
-                <!-- Add your menu items here -->
-            </div>
+    <div class="flexbox text-white">
+        <h1 class="text-2xl text-center font-bold my-4">Hero Class Menu</h1>
+            <hero-class></hero-class>
+            <hero-class></hero-class>
+            <hero-class></hero-class>
+            <hero-class></hero-class>
         </div>
     `,
-    styles: []
+    styles: [],
+    imports: [HeroClassComponent]
 })
-export class MainMenuComponent {}
+export class MainMenuComponent {
+    title = 'Main Menu';
+}
