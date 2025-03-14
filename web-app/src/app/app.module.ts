d731516a0a12d0160f725d76718a2b871d609b73
Alec <alecpierce19@gmail.com>
2025-03-13 20:29:40 -0500

    trying to get animations working

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { DialogMenuComponent } from './menus/dialog-menu.component';
 
 @NgModule({
   declarations: [
--- web-app/src/app/menus/dialog-menu.component.ts
+++ web-app/src/app/menus/dialog-menu.component.ts
@@ -3,13 +3,21 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Hero } from '../classes/hero';
 import { CommonModule } from '@angular/common';
 import { StatsComponent } from '../components/stats.component';
+import { bounceInUpOnEnterAnimation, zoomOutRightAnimation } from 'angular-animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @Component({
     selector: 'app-dialog-menu',
     templateUrl: './dialog-menu.component.html',
-    styleUrls: ['./dialog-menu.component.scss'],
-    imports: [CommonModule, StatsComponent]
+    imports: [CommonModule, StatsComponent, BrowserAnimationsModule],
+    animations: [zoomOutRightAnimation({ duration: 1000 }),
+      bounceInUpOnEnterAnimation({ anchor: 'enter1' })
+    ],
+    styleUrls: ['./dialog-menu.component.scss']
 })
 export class DialogMenuComponent {
+    animationState = false;
+    animationWithState = false;
+    hueBtnState = false;
     constructor(
         public dialogRef: MatDialogRef<DialogMenuComponent>,
         @Inject(MAT_DIALOG_DATA) public data: Hero
@@ -24,10 +32,18 @@ export class DialogMenuComponent {
         } else if (action === 'remove') {
           this.data.isInParty = false;
         }
-        this.dialogRef.close(action);
+        this.dialogRef.close();
       }
     
       closeDialog(): void {
         this.dialogRef.close();
+      }
+    
+      animate() {
+        this.animationState = false;
+        setTimeout(() => {
+          this.animationState = true;
+          this.animationWithState = !this.animationWithState;
+        }, 1);
       }
 }
