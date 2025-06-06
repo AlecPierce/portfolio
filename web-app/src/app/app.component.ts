f382a43dc5af9e82aa479d58178fde04df6ba03b
Alec <alecpierce19@gmail.com>
2025-06-06 12:52:10 -0500

    added routing

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { MainMenuComponent } from './menus/main-menu.component';
+import {RouterModule} from '@angular/router';
 
 @Component({
     selector: 'app-root',
     styleUrls: ['./app.component.scss'],
-    imports: [MainMenuComponent],
-    template: `<app-main-menu></app-main-menu>`
+    imports: [RouterModule],
+    template: `
+      <main>
+        <a [routerLink]="['/']">
+          <header class="brand-name">
+            <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+          </header>
+        </a>
+        <section class="content">
+          <router-outlet></router-outlet>
+        </section>
+      </main>
+    `
 })
 export class AppComponent {
   title = 'web-app';
 }
+
+//    template: `<app-main-menu></app-main-menu>`
--- web-app/src/main.ts
+++ web-app/src/main.ts
@@ -3,8 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import {provideRouter} from '@angular/router';
+import routeConfig from './app/routes';
 
 if (environment.production) {
   enableProdMode();
@@ -14,3 +16,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 bootstrapApplication(AppComponent);
+
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
