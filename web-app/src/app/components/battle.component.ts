17d4c7ce0d18177a2efa0781df3b7d8240cd0bc1
Alec <alecpierce19@gmail.com>
2025-06-09 00:20:25 -0500

    added _redirects

--- web-app/src/app/components/battle.component.ts
+++ web-app/src/app/components/battle.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
 import { Hero } from '../classes/hero';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'battle',
@@ -8,16 +9,21 @@ import { MatCardModule } from '@angular/material/card';
     <div class="bg-gray-700 p-4 rounded-lg shadow-lg w-64 h-auto">
         <mat-card class="rounded-lg p-10 bg-blue-600">
             <mat-card-header>{{battleShout()}}</mat-card-header>
-            <mat-card-content>
-                <audio autoplay [volume]="0.25">
-                    <source src="../assets/time-of-bliss-212074.mp3" type="audio/mp3">
+            <mat-card-content *ngIf="justMakoto">
+                <audio autoplay loop [volume]="0.25">
+                    <source src="../assets/persona3-mass-destruction.mp3" type="audio/mp3">
+                </audio>
+            </mat-card-content>
+            <mat-card-content *ngIf="!justMakoto">
+                <audio autoplay loop [volume]="0.25">
+                    <source src="../assets/persona4-junes.mp3" type="audio/mp3">
                 </audio>
             </mat-card-content>
         </mat-card>
     </div>`,
     standalone: true,
     styleUrls: [],
-    imports: [MatCardModule]
+    imports: [MatCardModule, CommonModule]
 })
 export class BattleComponent {
 
@@ -26,8 +32,10 @@ export class BattleComponent {
 
     title = 'Battle Component';
     heroesString: string = '';
+    justMakoto: boolean = false;
 
     battleShout(): string {
+        this.justMakoto = this.heroes[0] === 'Makoto';
         this.heroesString = this.heroes.join(', ');
         return this.heroesString + ' is ready to battle!';
     }
