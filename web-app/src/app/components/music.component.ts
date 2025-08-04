2678f5dcf2f9145961533933dd7c4382bf0b512a
Alec <alecpierce19@gmail.com>
2025-08-04 17:25:02 -0500

    fixed volume issues with music player and added disabled buttons when 100 or 0

--- web-app/src/app/components/music.component.ts
+++ web-app/src/app/components/music.component.ts
@@ -1,66 +1,62 @@
 import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'music',
-  template: `
+    selector: 'music',
+    template: `
   <div class="grid justify-items-center text-white">
     <div class="music-src-title px-2">
         {{musicSrcTitle}}
     </div>   
     @if(musicOn) { 
-    <audio autoplay loop [volume]="musicVolume">
+    <audio autoplay loop [volume]="calcMusicVolume(musicVolume)">
         <source [src]="musicSrc" type="audio/mp3">
     </audio>
     }
 
 
     <div class="px-2">
-        {{calcMusicVolume(musicVolume)}}%
+        Volume: {{musicVolume}}%
     </div>
     <div>
-    <button type="button" (click)="increaseMusicVolume()"
-    class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mw-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-        +</button>
-    <button type="button" (click)="decreaseMusicVolume()"
-    class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mw-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-        -</button>
+        @if(musicVolume < 100) {
+            <button type="button" (click)="increaseMusicVolume()"
+                class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mw-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            +</button>
+        }
+        @if(musicVolume == 100) {
+            <button type="button" (click)="increaseMusicVolume()"
+                disabled class="cursor-not-allowed text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">
+            +</button>
+        }
+        @if(musicVolume > 0) {
+            <button type="button" (click)="decreaseMusicVolume()"
+                class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mw-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            -</button>
+        }
+        @if(musicVolume == 0) {
+            <button type="button" (click)="decreaseMusicVolume()"
+                disabled class="cursor-not-allowed text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">
+            -</button>
+        }
     </div>
 
-    <!-- need to fix bug where -10% shows up, should have disabled - -->
-    <!-- need to fix bug where it goes from 80% to 89%, 99%, and allows 109% which + should get disabled when at 100% or more -->
     <div>
     @if(musicOn) {
-        @if(musicVolume > 0) {
-            <button type="button" (click)="toggleMusic()"
+        <button type="button" (click)="toggleMusic()"
             class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-            Stop</button>
-        }
-        @if(musicVolume == 0) {
-            <!-- disabled -->
-            <button type="button" disabled
-            class="cursor-not-allowed text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">
-            Stop</button>
-        }
+        Stop</button>
     }
     @if(!musicOn) {
-        @if(musicVolume > 0) {
-            <button type="button" (click)="toggleMusic()"
+        <button type="button" (click)="toggleMusic()"
             class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-            Start</button>
-        }
-        @if(musicVolume == 0) {
-            <!-- disabled -->
-            <button type="button" disabled
-            class="cursor-not-allowed text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">
-            Start</button>
-        }
+        Start</button>
     }
     </div>
   </div>
 `,
-  styleUrl: 'music.component.css',
-  standalone: true,
-  imports: [],
+    styleUrl: 'music.component.css',
+    standalone: true,
+    imports: [],
 })
 export class musicComponent {
     @Input({ required: true })
@@ -68,19 +64,26 @@ export class musicComponent {
     @Input({ required: true })
     musicSrcTitle: string = '';
 
-    musicVolume: number = 0.20;
+    musicVolume: number = 20;
+    // calcdMusicVolume: number = 20;
     musicOn: boolean = true;
 
     increaseMusicVolume() {
-        if (this.musicVolume < 1) {
-            this.musicVolume += 0.1;
+        if (this.musicVolume == 100) {
+            return;
+        } else if (this.musicVolume < 100) {
+            this.musicVolume += 5;
         }
+        // this.calcdMusicVolume = this.calcMusicVolume(this.musicVolume);
     }
 
     decreaseMusicVolume() {
-        if (this.musicVolume > 0) {
-            this.musicVolume -= 0.1;
+        if (this.musicVolume == 0) {
+            return;
+        } else if (this.musicVolume > 0) {
+            this.musicVolume -= 5;
         }
+        // this.calcdMusicVolume = this.calcMusicVolume(this.musicVolume);
     }
 
     toggleMusic() {
@@ -88,6 +91,11 @@ export class musicComponent {
     }
 
     calcMusicVolume(musicVolume: number): number {
-        return Math.floor(musicVolume * 100);
+        let calcdMusicVolume = musicVolume / 100;
+        // if (calcdMusicVolume % 5 != 0) {
+        //     (musicVolume * 100) - calcdMusicVolume > 0 ? calcdMusicVolume++ : calcdMusicVolume--;
+        //     return calcdMusicVolume;
+        // }
+        return calcdMusicVolume;
     }
 }
