843c9a0c5f21a156ebf87faa5d95799695883200
Alec <alecpierce19@gmail.com>
2025-08-04 17:25:32 -0500

    formatting

--- web-app/src/app/components/music.component.ts
+++ web-app/src/app/components/music.component.ts
@@ -3,56 +3,56 @@ import { Component, Input } from "@angular/core";
 @Component({
     selector: 'music',
     template: `
-  <div class="grid justify-items-center text-white">
-    <div class="music-src-title px-2">
-        {{musicSrcTitle}}
-    </div>   
-    @if(musicOn) { 
-    <audio autoplay loop [volume]="calcMusicVolume(musicVolume)">
-        <source [src]="musicSrc" type="audio/mp3">
-    </audio>
-    }
+    <div class="grid justify-items-center text-white">
+        <div class="music-src-title px-2">
+            {{musicSrcTitle}}
+        </div>   
+        @if(musicOn) { 
+        <audio autoplay loop [volume]="calcMusicVolume(musicVolume)">
+            <source [src]="musicSrc" type="audio/mp3">
+        </audio>
+        }
 
 
-    <div class="px-2">
-        Volume: {{musicVolume}}%
-    </div>
-    <div>
-        @if(musicVolume < 100) {
-            <button type="button" (click)="increaseMusicVolume()"
-                class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mw-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-            +</button>
-        }
-        @if(musicVolume == 100) {
-            <button type="button" (click)="increaseMusicVolume()"
-                disabled class="cursor-not-allowed text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">
-            +</button>
-        }
-        @if(musicVolume > 0) {
-            <button type="button" (click)="decreaseMusicVolume()"
-                class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mw-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-            -</button>
-        }
-        @if(musicVolume == 0) {
-            <button type="button" (click)="decreaseMusicVolume()"
-                disabled class="cursor-not-allowed text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">
-            -</button>
-        }
-    </div>
+        <div class="px-2">
+            Volume: {{musicVolume}}%
+        </div>
+        <div>
+            @if(musicVolume < 100) {
+                <button type="button" (click)="increaseMusicVolume()"
+                    class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mw-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                +</button>
+            }
+            @if(musicVolume == 100) {
+                <button type="button" (click)="increaseMusicVolume()"
+                    disabled class="cursor-not-allowed text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">
+                +</button>
+            }
+            @if(musicVolume > 0) {
+                <button type="button" (click)="decreaseMusicVolume()"
+                    class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mw-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                -</button>
+            }
+            @if(musicVolume == 0) {
+                <button type="button" (click)="decreaseMusicVolume()"
+                    disabled class="cursor-not-allowed text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600">
+                -</button>
+            }
+        </div>
 
-    <div>
-    @if(musicOn) {
-        <button type="button" (click)="toggleMusic()"
-            class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-        Stop</button>
-    }
-    @if(!musicOn) {
-        <button type="button" (click)="toggleMusic()"
-            class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-        Start</button>
-    }
+        <div>
+        @if(musicOn) {
+            <button type="button" (click)="toggleMusic()"
+                class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            Stop</button>
+        }
+        @if(!musicOn) {
+            <button type="button" (click)="toggleMusic()"
+                class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            Start</button>
+        }
+        </div>
     </div>
-  </div>
 `,
     styleUrl: 'music.component.css',
     standalone: true,
